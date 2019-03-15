a7ba56a6482d5041b1fd04fd0dbc53f257381c0d
Guest User <Guest@epicodus-26.local>
2019-03-15 16:34:50 -0700

    adds controller tests

---
+++ HairSalon.Tests/ControllerTests/ClientControllerTests.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using HairSalon.Controllers;
+using HairSalon.Models;
+
+namespace HairSalon.TestTools
+{
+    [TestClass]
+    public class ClientsControllerTest
+    {
+        [TestMethod]
+        public void Create_ReturnsCorrectActionType_ViewResult()
+        {
+             //Arrange
+             ClientsController controller = new ClientsController();
+
+             //Act 
+             IActionResult view = controller.Create("Walk the dog");
+
+             //Assert
+             Assert.IsInstanceOfType(view, typeof(ViewResult));
+        }
+    }
+}
